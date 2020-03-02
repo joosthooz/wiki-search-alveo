9a1e16cfd27390e14b9eb90de86a63b7e3b5e567
Kati Tervo <kati.tervo@tuni.fi>
2020-03-02 15:50:40 +0100

    Fix tta_wrapper_resize

--- hardware/tta_support_rtl/tta_wrapper_resize.vhdl
+++ hardware/tta_support_rtl/tta_wrapper_resize.vhdl
@@ -1,7 +1,24 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use tta_wrapper_resize_pkg.all;
+
+entity tta_wrapper_resize is
+  port (
+    clk, reset  : in std_logic;
+    in_valid    : in  std_logic;
+    in_ready    : out std_logic;
+    in_data     : in  std_logic_vector(255 downto 0);
+    in_cnt      : in  std_logic_vector(4 downto 0);
+    in_last     : in  std_logic;
+
+    out_valid   : out std_logic;
+    out_ready   : in  std_logic;
+    out_dvalid  : out std_logic;
+    out_data    : out std_logic_vector(255 downto 0);
+    out_cnt     : out std_logic_vector(4 downto 0);
+    out_last    : out std_logic
+  );
+end entity tta_wrapper_resize;
 
 architecture rtl of tta_wrapper_resize is
     signal narrow_in_valid, narrow_in_ready, narrow_in_last,
@@ -14,8 +31,8 @@ begin
   down_gearbox : entity work.StreamGearbox
   generic map (
     ELEMENT_WIDTH => 8,
-    IN_COUNT_MAX => 8,
-    IN_COUNT_WIDTH => 3,
+    IN_COUNT_MAX => 32,
+    IN_COUNT_WIDTH => 5,
 
     OUT_COUNT_MAX  => 1,
     OUT_COUNT_WIDTH => 1
@@ -42,8 +59,8 @@ begin
     IN_COUNT_MAX => 1,
     IN_COUNT_WIDTH => 1,
 
-    OUT_COUNT_MAX  => 8,
-    OUT_COUNT_WIDTH => 4
+    OUT_COUNT_MAX  => 32,
+    OUT_COUNT_WIDTH => 5
   ) port map(
     clk => clk, reset => reset,
     
