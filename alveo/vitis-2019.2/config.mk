b2e72a250076e538f535d6c78a29fecce6bbd5e4
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2020-09-29 09:23:40 +0200

    Added unisim source to vhdeps scripts

--- alveo/vitis-2019.2/config.mk
+++ alveo/vitis-2019.2/config.mk
@@ -3,7 +3,8 @@ VHDL_FILES = $(shell python3 -m vhdeps dump krnl_word_match_rtl \
                -i ../../hardware/vhdl \
                -i ../../hardware/fletcher/hardware \
                -i ../../hardware/tta_rtl \
-	       -i ../../hardware/tta_support_rtl \
+               -x unisim:$(XILINX_VIVADO)/data/vhdl/src/unisims/unisim_VCOMP.vhd
+               -i ../../hardware/tta_support_rtl \
                -msyn -v93 | cut -d ' ' -f 4-)
 
 VIVADO := $(XILINX_VIVADO)/bin/vivado
--- hardware/run-vhdeps.sh
+++ hardware/run-vhdeps.sh
@@ -1,1 +1,1 @@
-vhdeps "$@" -i vhdl -i fletcher/hardware -i tta_rtl -i tta_support_rtl
+vhdeps "$@" -i vhdl -i fletcher/hardware -i tta_rtl -i tta_support_rtl -x unisim:$XILINX_VIVADO/data/vhdl/src/unisims/unisim_VCOMP.vhd
