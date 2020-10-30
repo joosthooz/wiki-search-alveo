da65b72b8c253a46d136b7bd1a51f40eca108afb
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2020-10-30 22:30:50 +0100

    Using 1clk FIFO to allow infering URAMs, some code cleanup.
    This relies on 1clk variants of StreamFIFO and UtilRam1R1W to be added to vhlib in fletcher.

--- hardware/tta_support_rtl/tta_wrapper_resize.vhdl
+++ hardware/tta_support_rtl/tta_wrapper_resize.vhdl
@@ -27,30 +27,38 @@ architecture rtl of tta_wrapper_resize is
            narrow_out_valid, narrow_out_ready, narrow_out_dvalid, narrow_out_last : std_logic;
     signal narrow_in_cnt, narrow_out_cnt : std_logic_vector(1 downto 0);
     signal narrow_in_data, narrow_out_data : std_logic_vector(32-1 downto 0);
-
-
-    signal ififo2tta_all : std_logic_vector(67 downto 0); -- last, cnt(3), data
-    signal tta2ofifo_all : std_logic_vector(69 downto 0); --dvalid, last, cnt(4), data
-    signal out_all : std_logic_vector(69 downto 0);
-    signal ififo2tta_data : std_logic_vector(63 downto 0);
-    signal tta2ofifo_data : std_logic_vector(63 downto 0);
-    signal ififo2tta_cnt : std_logic_vector(2 downto 0);
-    signal tta2ofifo_cnt : std_logic_vector(3 downto 0);
-    signal ififo2tta_ready : std_logic;
-    signal tta2ofifo_ready : std_logic;
+    
     signal ififo2tta_valid : std_logic;
-    signal tta2ofifo_valid : std_logic;
-    signal ififo2tta_last : std_logic;
-    signal tta2ofifo_last : std_logic;
-    signal tta2ofifo_dvalid : std_logic;
+    signal ififo2tta_ready : std_logic;
+    signal ififo2tta_all : std_logic_vector(67 downto 0); -- last, cnt(3), data
+    signal ififo2tta_valid_sliced : std_logic;
+    signal ififo2tta_ready_sliced : std_logic; 
+    signal ififo2tta_cnt_sliced     : std_logic_vector(2 downto 0);
+    signal ififo2tta_last_sliced    : std_logic;
+    signal ififo2tta_data_sliced : std_logic_vector(63 downto 0);
+    signal ififo2tta_all_sliced : std_logic_vector(67 downto 0); -- last, cnt(3),
+    
+    signal tta2ofifo_valid   : std_logic;
+    signal tta2ofifo_ready   : std_logic;
+    signal tta2ofifo_cnt     : std_logic_vector(3 downto 0);
+    signal tta2ofifo_last    : std_logic;
+    signal tta2ofifo_dvalid  : std_logic;
+    signal tta2ofifo_data    : std_logic_vector(63 downto 0);
+    signal tta2ofifo_all     : std_logic_vector(69 downto 0); --dvalid, last, cnt(4), data
+    
+    signal ofifo2slice_valid : std_logic;
+    signal ofifo2slice_ready : std_logic;
+    signal ofifo2slice_all   : std_logic_vector(69 downto 0); --dvalid, last, cnt(4), data
+    
+    signal out_all           : std_logic_vector(69 downto 0);
 begin
 
   --FIFOs at the in and output so that multiple instances can operate concurrently
-  input_FIFO: StreamFIFO
+  input_FIFO: StreamFIFO_1clk
     generic map (
       DEPTH_LOG2  => 13,
       DATA_WIDTH  => 64 + 3 + 1,
-      RAM_CONFIG  => "URAM"
+      RAM_CONFIG  => "ULTRA"
     )
     port map (
       in_clk    => clk,
@@ -66,14 +74,35 @@ begin
       out_ready => ififo2tta_ready,
       out_data  => ififo2tta_all
     );
-  ififo2tta_last <= ififo2tta_all(67);
-  ififo2tta_cnt  <= ififo2tta_all(66 downto 64);
-  ififo2tta_data <= ififo2tta_all(63 downto 0);
 
-  output_FIFO: StreamFIFO
+  
+  inputslice: StreamSlice
+  generic map (
+    DATA_WIDTH => 68
+  )
+  port map (
+      clk   => clk,
+      reset => reset,
+
+      in_valid => ififo2tta_valid,
+      in_ready => ififo2tta_ready,
+      in_data  => ififo2tta_all,
+      
+      out_valid => ififo2tta_valid_sliced,
+      out_ready => ififo2tta_ready_sliced,
+      out_data  => ififo2tta_all_sliced
+  );
+
+  ififo2tta_last_sliced <= ififo2tta_all_sliced(67);
+  ififo2tta_cnt_sliced  <= ififo2tta_all_sliced(66 downto 64);
+  ififo2tta_data_sliced <= ififo2tta_all_sliced(63 downto 0);
+  tta2ofifo_all <= tta2ofifo_dvalid & tta2ofifo_last & tta2ofifo_cnt & tta2ofifo_data;
+
+  output_FIFO: StreamFIFO_1clk
     generic map (
       DEPTH_LOG2  => 13,
-      DATA_WIDTH  => 64 + 4 + 1 + 1
+      DATA_WIDTH  => 64 + 4 + 1 + 1,
+      RAM_CONFIG  => "ULTRA"
     )
     port map (
       in_clk    => clk,
@@ -85,15 +114,32 @@ begin
       out_clk   => clk,
       out_reset => reset,
 
+      out_valid => ofifo2slice_valid,
+      out_ready => ofifo2slice_ready,
+      out_data  => ofifo2slice_all
+    );
+  
+  
+  outputslice: StreamSlice
+  generic map (
+    DATA_WIDTH => 70
+  )
+  port map (
+      clk   => clk,
+      reset => reset,
+
+      in_valid => ofifo2slice_valid,
+      in_ready => ofifo2slice_ready,
+      in_data  => ofifo2slice_all,
+      
       out_valid => out_valid,
       out_ready => out_ready,
       out_data  => out_all
-    );
-  tta2ofifo_all <= tta2ofifo_dvalid & tta2ofifo_last & tta2ofifo_cnt & tta2ofifo_data;
+  );
   out_dvalid <= out_all(69);
-  out_last <= out_all(68);
-  out_cnt  <= out_all(67 downto 64);
-  out_data <= out_all(63 downto 0);
+  out_last   <= out_all(68);
+  out_cnt    <= out_all(67 downto 64);
+  out_data   <= out_all(63 downto 0);
 
   down_gearbox : entity work.StreamGearbox
   generic map (
@@ -106,11 +152,11 @@ begin
   ) port map(
     clk => clk, reset => reset,
     
-    in_valid => ififo2tta_valid,
-    in_ready => ififo2tta_ready, 
-    in_data  => ififo2tta_data,
-    in_count => ififo2tta_cnt,
-    in_last  => ififo2tta_last,
+    in_valid => ififo2tta_valid_sliced,
+    in_ready => ififo2tta_ready_sliced, 
+    in_data  => ififo2tta_data_sliced,
+    in_count => ififo2tta_cnt_sliced,
+    in_last  => ififo2tta_last_sliced,
 
     -- Output stream.
     out_valid => narrow_in_valid, 
